0dd6bfed71f18daa8af2935c58005a63c0e0a7a3
Replit user <>
2023-11-06 17:26:18 +0000

    add ball bounce functionality gh

---
+++ ball.py
@@ -0,0 +1,23 @@
+from turtle import Turtle
+
+class Ball(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.shapesize(stretch_wid=1,stretch_len=1)
+        self.color("white")
+        self.penup()
+        self.speed("fastest")
+        self.goto(0, 0)
+        self.x_move = 10
+        self.y_move = 10
+        
+    def move(self):
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
+        self.goto(new_x, new_y)
+
+    def bounce(self):
+        #new_y = self.ycor() - self.y_move
+        #self.goto(self.xcor(), new_y)
+        self.y_move *= -1
