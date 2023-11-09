846470682e55c8dc2e6464b1a7ec1614550182ee
ad3rinto <aad3rinto@gmail.com>
2023-11-09 11:17:53 +0000

    add ball bounce off paddles functionality

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
-from ball  import Ball
+from ball import Ball
 import time
 
 screen = Screen()
@@ -12,7 +12,6 @@ right_paddle = Paddle((350, 0))
 left_paddle = Paddle((-350, 0))
 ball = Ball()
 
-
 screen.listen()
 screen.onkey(right_paddle.move_up, "Up")
 screen.onkey(right_paddle.move_down, "Down")
@@ -21,12 +20,17 @@ screen.onkey(left_paddle.move_down, "z")
 
 game_on = True
 while game_on:
-  screen.update()
-  time.sleep(0.1)
-  ball.move()
+    screen.update()
+    time.sleep(0.1)
+    ball.move()
 
-#detect collision
-  if ball.ycor() > 280 or ball.ycor() < -280:
-    ball.bounce()
+    # detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # detect collision with paddles
+    if ball.distance(right_paddle) < 50 and ball.xcor() > 340 or ball.distance(left_paddle) < 50 and ball.xcor() < -340:
+        ball.bounce_x()
+
 
 screen.exitonclick()
